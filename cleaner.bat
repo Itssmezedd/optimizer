30345f559717ac6c8408f9e9cd8c276ae7b7e2ac
John Cedric Bautista <103129301+Itssmezedd@users.noreply.github.com>
2023-07-18 14:34:15 +0800

    file

--- cleaner.bat
+++ cleaner.bat
@@ -5,8 +5,8 @@ goto :main
 
 :main
 rem CLEAN PC:
-rd /s /q C:\Windows\Prefetch\
-rd /s /q C:\Windows\Temp\
-rd /s /q %userprofile%\appdata\local\temp\
+rd /s /q C:\Windows\Prefetch\*
+rd /s /q C:\Windows\Temp\*
+rd /s /q %userprofile%\appdata\local\temp\*
 exit
 goto :eof
